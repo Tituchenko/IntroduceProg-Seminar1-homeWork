cb51993d7163ca3bcc0ea8d80a75749c16b1d950
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-16 16:39:40 +0300

    Ex 4 solved

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
-*/
+
 Console.Write("Введите первое число: ");
 int a=int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
@@ -22,13 +22,26 @@ else
 
 
 
-/*
+
 Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
 2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22
 */
+Console.Write("Введите первое число: ");
+int a=int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b=int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c=int.Parse(Console.ReadLine());
+
+int max=a;
+if (b>max) max=b; 
+if (c>max) max=c;
+Console.WriteLine(max);
+
+
 
 /*
 Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
